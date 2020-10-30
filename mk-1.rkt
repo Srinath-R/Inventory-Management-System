c424d1649ddaa9d58c23f8a2a5d79443807b2bb8
Srinath R <35762713+Srinath-R@users.noreply.github.com>
2020-10-30 23:27:12 +0530

    Add files via upload

--- mk-1.rkt
+++ mk-1.rkt
@@ -1,2 +1,11 @@
 #lang racket
 (require racket/class)
+
+(define book-class%
+  (class object%
+    (field (pages 5))
+    (define/public (letters)
+      (* pages 500))
+    (super-new)))
+
+(send (new book-class%) letters)
